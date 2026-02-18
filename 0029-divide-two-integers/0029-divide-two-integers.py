1dd52b81d960b1b4057542000456bffef762de01
siva kumar <146540716+SivaKumar5757@users.noreply.github.com>
2026-02-18 18:36:54 +0530

    Time: 3 ms (19.69%), Space: 19.4 MB (20.92%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.py
+++ 0029-divide-two-integers/0029-divide-two-integers.py
@@ -3,4 +3,15 @@ class Solution:
         if dividend==-2147483648 and divisor==-1 :
             return 2147483647
 
-        return int(dividend/divisor)
+        neg=(dividend<0)^(divisor<0)
+        dividend,divisor=abs(dividend),abs(divisor)
+        q=0
+        while divisor <= dividend:
+            mul=1
+            temp=divisor
+            while dividend>=(temp<<1):
+                temp<<=1
+                mul<<=1
+            q+=mul
+            dividend-=temp
+        return q if not neg else -q
